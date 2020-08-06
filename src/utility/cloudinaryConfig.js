f687cd4f4843d21e50baae32fb4e6169beae4837
Vicman2 <vicmanthebest@gmail.com>
2020-08-06 15:03:42 +0000

    Added image to products

--- src/utility/cloudinaryConfig.js
+++ src/utility/cloudinaryConfig.js
@@ -1,7 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const config = require('../config/constants');
 const { cloudinaryApiKey } = require('../config/constants');
-const path = require('path')
 
 const uploadToCloudinary = async(fileName, pathToFile)=>{
     cloudinary.config({
@@ -9,7 +8,7 @@ const uploadToCloudinary = async(fileName, pathToFile)=>{
         api_key:cloudinaryApiKey ,
         api_secret: config.cloudinaryApiSecret,
     })
-    const image =  await cloudinary.uploader.upload(pathToFile, {public_id: fileName, folder: path.join(__dirname + 'products')})
+    const image =  await cloudinary.uploader.upload(pathToFile, {public_id: fileName, folder: 'products'})
     return image
 }
 
